444523aa0a406a881072c691b903d8d674382dd6
zakkturner <30674356+zakkturner@users.noreply.github.com>
2020-05-22 00:00:50 -0400

    Refactored to BEM and Styled mobile version of sign up

--- src/components/organisms/Sign-Up.jsx
+++ src/components/organisms/Sign-Up.jsx
@@ -5,17 +5,31 @@ import "./Sign-Up.styles.scss";
 export default function SignUp() {
   return (
     <main className="sign-up">
-      <h1 className="title">join the list</h1>
-      <span className="sub-title">Sign Up for</span>
-      <span className="sub-title">The IN-Dex Newsletter</span>
-      <form>
-        <div className="form-group">
-          <input type="email" name="email" className="form-input" required />
-          <input type="submit" name="submit" className="submit-btn">
-            Next
-          </input>
+      <h1 className="sign-up__title">join the list</h1>
+      <span className="sign-up__sub-title">Sign Up for</span>
+      <span className="sign-up__sub-title">The IN-Dex Newsletter</span>
+      <form className="form">
+        <div className="form__group">
+          <input
+            type="text"
+            name="email"
+            className="form__group--input"
+            placeholder="Enter Email Address"
+          />
+          <input
+            type="submit"
+            name="submit"
+            className="form__group--submit"
+            value="Next"
+          />
         </div>
-        <input type="checkbox" name="policy" className="policy" />
+        <div className="policy">
+          <input type="checkbox" name="policy" className="policy__box" />
+          <blockquote className="policy__text">
+            I agree to receive information from Interactive Nerd in accordance
+            with the following <a href="#">Privacy Policy</a>
+          </blockquote>
+        </div>
       </form>
     </main>
   );
